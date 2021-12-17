b04cedd98cfd682f9307233c44e9d56af1acd538
michael <michalfeliks19@gmail.com>
2021-12-17 11:47:26 +0100

    Upgrade layer

--- app/src/main/java/com/example/mobilizeyourplc/ControlModule.java
+++ app/src/main/java/com/example/mobilizeyourplc/ControlModule.java
@@ -1,19 +1,22 @@
 package com.example.mobilizeyourplc;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class ControlModule extends Activity implements View.OnClickListener {
+
+    EditText inputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control_module);
+
+        inputText = (EditText) findViewById(R.id.input);
 
         Button eight = (Button) findViewById(R.id.button6);
         eight.setOnClickListener(this); // calling onClick() method
@@ -31,6 +34,9 @@ public class ControlModule extends Activity implements View.OnClickListener {
             case R.id.button9:
                 intent = new Intent(this, MainActivity2.class);
                 startActivity(intent);
+                break;
+            case R.id.send:
+                // To DO
                 break;
             default:
                 break;
